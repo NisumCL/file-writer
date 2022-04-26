8edbd6b888184c141157ee63fabc3e658d9e34c7
Beatriz <lopezcasanovabeatriz@gmail.com>
2022-04-26 15:56:04 -0400

    add file File-writer

--- __tests__/unit/writers/text-writer.spec.js
+++ __tests__/unit/writers/text-writer.spec.js
@@ -1,111 +1,136 @@
-const { textWriter } = require("../../../writers/text-writer")
+const { textWriter } = require("../../../writers/text-writer");
 const { parse, join } = require("path");
-const { existsSync, accessSync, writeFileSync, constants: { W_OK } } = require("fs");
+const {
+  existsSync,
+  accessSync,
+  writeFileSync,
+  constants: { W_OK },
+} = require("fs");
 const { slugify } = require("../../../helpers");
+const { hasWriteAccess, writeFile } = require("../../../helpers/file-helper");
 
-jest.mock("path")
-jest.mock("fs")
-jest.mock("../../../helpers")
+jest.mock("path");
+jest.mock("fs");
+jest.mock("../../../helpers");
 
-afterEach(() => jest.clearAllMocks())
+afterEach(() => jest.clearAllMocks());
 
 it("Should throw an error when filename couldn't be parse", () => {
-    parse.mockImplementation(() => { throw new TypeError("Some parse error"); })
+  parse.mockImplementation(() => {
+    throw new TypeError("Some parse error");
+  });
 
-    expect(() => { textWriter(null, "Hello world!") }).toThrowError("Invalid filename argument")
+  expect(() => {
+    textWriter(null, "Hello world!");
+  }).toThrowError("Invalid filename argument");
 
-    expect(parse).toBeCalledWith(null)
-    expect(slugify).not.toBeCalled()
-    expect(join).not.toBeCalled()
-    expect(existsSync).not.toBeCalled()
-    expect(accessSync).not.toBeCalled()
-    expect(writeFileSync).not.toBeCalled()
-})
+  expect(parse).toBeCalledWith(null);
+  expect(slugify).not.toBeCalled();
+  expect(join).not.toBeCalled();
+  expect(existsSync).not.toBeCalled();
+  expect(accessSync).not.toBeCalled();
+  expect(writeFileSync).not.toBeCalled();
+});
 
 it("Should throw an error when given an invalid filename", () => {
-    parse.mockReturnValueOnce({ name:"", ext:"", dir:"" })
-    slugify.mockImplementation(() => { throw new TypeError("Some slugify error"); })
+  parse.mockReturnValueOnce({ name: "", ext: "", dir: "" });
+  slugify.mockImplementation(() => {
+    throw new TypeError("Some slugify error");
+  });
 
-    expect(() => { textWriter("", "Hello world!") }).toThrowError("Invalid filename argument")
+  expect(() => {
+    textWriter("", "Hello world!");
+  }).toThrowError("Invalid filename argument");
 
-    expect(parse).toBeCalledWith("")
-    expect(slugify).toBeCalledWith("")
-    expect(join).not.toBeCalled()
-    expect(existsSync).not.toBeCalled()
-    expect(accessSync).not.toBeCalled()
-    expect(writeFileSync).not.toBeCalled()
-})
+  expect(parse).toBeCalledWith("");
+  expect(slugify).toBeCalledWith("");
+  expect(join).not.toBeCalled();
+  expect(existsSync).not.toBeCalled();
+  expect(accessSync).not.toBeCalled();
+  expect(writeFileSync).not.toBeCalled();
+});
 
 it("Should throw an error when given an invalid path or doesn't exists", () => {
-    parse.mockReturnValueOnce({ name:"new file", ext:".txt", dir:"/tmp" })
-    slugify.mockReturnValueOnce("new-file")
-    join.mockReturnValueOnce("/tmp/new-file.txt")
-    existsSync.mockReturnValueOnce(false)
+  parse.mockReturnValueOnce({ name: "new file", ext: ".txt", dir: "/tmp" });
+  slugify.mockReturnValueOnce("new-file");
+  join.mockReturnValueOnce("/tmp/new-file.txt");
+  existsSync.mockReturnValueOnce(false);
 
-    expect(() => { textWriter("/tmp/new file.txt", "Hello world!") }).toThrowError("Invalid path argument")
+  expect(() => {
+    textWriter("/tmp/new file.txt", "Hello world!");
+  }).toThrowError("Invalid path argument");
 
-    expect(parse).toBeCalledWith("/tmp/new file.txt")
-    expect(slugify).toBeCalledWith("new file")
-    expect(join).toBeCalledWith("/tmp","new-file.txt")
-    expect(existsSync).toBeCalledWith("/tmp")
-    expect(accessSync).not.toBeCalled()
-    expect(writeFileSync).not.toBeCalled()
-})
+  expect(parse).toBeCalledWith("/tmp/new file.txt");
+  expect(slugify).toBeCalledWith("new file");
+  expect(join).toBeCalledWith("/tmp", "new-file.txt");
+  expect(existsSync).toBeCalledWith("/tmp");
+  expect(accessSync).not.toBeCalled();
+  expect(writeFileSync).not.toBeCalled();
+});
 
 it("Should throw an error when it don't have write permissions on the given path", () => {
-    parse.mockReturnValueOnce({ name:"new file", ext:".txt", dir:"/tmp" })
-    slugify.mockReturnValueOnce("new-file")
-    join.mockReturnValueOnce("/tmp/new-file.txt")
-    existsSync.mockReturnValueOnce(true)
-    accessSync.mockImplementation(() => { throw new TypeError("Some accessSync error"); })
+  parse.mockReturnValueOnce({ name: "new file", ext: ".txt", dir: "/tmp" });
+  slugify.mockReturnValueOnce("new-file");
+  join.mockReturnValueOnce("/tmp/new-file.txt");
+  existsSync.mockReturnValueOnce(true);
+  accessSync.mockImplementation(() => {
+    throw new TypeError("Some accessSync error");
+  });
 
-    // expect(() => { hasWriteAccess('/tmpp/new-file.txt') }).toBe(false)
-    expect(() => { textWriter("/tmp/new file.txt", 'Hello world') }).toThrowError("It doesn't have write permissions")
+  // expect(() => { hasWriteAccess('/tmpp/new-file.txt') }).toBe(false)
+  expect(() => {
+    textWriter("/tmp/new file.txt", "Hello world");
+  }).toThrowError("It doesn't have write permissions");
 
-    expect(parse).toBeCalledWith("/tmp/new file.txt")
-    expect(slugify).toBeCalledWith("new file")
-    expect(join).toBeCalledWith("/tmp","new-file.txt")
-    expect(existsSync).toBeCalledWith("/tmp")
+  expect(parse).toBeCalledWith("/tmp/new file.txt");
+  expect(slugify).toBeCalledWith("new file");
+  expect(join).toBeCalledWith("/tmp", "new-file.txt");
+  expect(existsSync).toBeCalledWith("/tmp");
 
-    expect(accessSync).toBeCalledWith('/tmp', W_OK)
-    expect(writeFileSync).not.toBeCalled()
-})
+  expect(accessSync).toBeCalledWith("/tmp", W_OK);
+  expect(writeFileSync).not.toBeCalled();
+});
 
 it("Should throw an error when it couldn't write the file", () => {
-    parse.mockReturnValueOnce({ name:"new file", ext:".txt", dir:"/tmp" })
-    slugify.mockReturnValueOnce("new-file")
-    join.mockReturnValueOnce("/tmp/new-file.txt")
-    existsSync.mockReturnValueOnce(true)
-    accessSync.mockReturnValueOnce(true)
-    writeFileSync.mockImplementation(() => { throw new TypeError("writeFileSync"); })
+  parse.mockReturnValueOnce({ name: "new file", ext: ".txt", dir: "/tmp" });
+  slugify.mockReturnValueOnce("new-file");
+  join.mockReturnValueOnce("/tmp/new-file.txt");
+  existsSync.mockReturnValueOnce(true);
+  accessSync.mockReturnValueOnce(true);
+  writeFileSync.mockImplementation(() => {
+    throw new TypeError("writeFileSync");
+  });
 
-    // expect(() => { hasWriteAccess('/tmpp/new-file.txt') }).toBe(false)
-    expect(() => { textWriter("/tmp/new file.txt", 'Hello world') }).toThrowError("Ups! something wrong happened while writing the file")
+  // expect(() => { hasWriteAccess('/tmpp/new-file.txt') }).toBe(false)
+  expect(() => {
+    textWriter("/tmp/new file.txt", "Hello world");
+  }).toThrowError("Ups! something wrong happened while writing the file");
 
-    expect(parse).toBeCalledWith("/tmp/new file.txt")
-    expect(slugify).toBeCalledWith("new file")
-    expect(join).toBeCalledWith("/tmp","new-file.txt")
-    expect(existsSync).toBeCalledWith("/tmp")
+  expect(parse).toBeCalledWith("/tmp/new file.txt");
+  expect(slugify).toBeCalledWith("new file");
+  expect(join).toBeCalledWith("/tmp", "new-file.txt");
+  expect(existsSync).toBeCalledWith("/tmp");
 
-    expect(accessSync).toBeCalledWith('/tmp', W_OK)
-    expect(writeFileSync).toBeCalledWith("/tmp/new-file.txt", 'Hello world')
-})
+  expect(accessSync).toBeCalledWith("/tmp", W_OK);
+  expect(writeFileSync).toBeCalledWith("/tmp/new-file.txt", "Hello world");
+});
 
 it("Should write a text file succeesfully", () => {
-    parse.mockReturnValueOnce({ name:"new file", ext:".txt", dir:"/tmp" })
-    slugify.mockReturnValueOnce("new-file")
-    join.mockReturnValueOnce("/tmp/new-file.txt")
-    existsSync.mockReturnValueOnce(true)
-    accessSync.mockReturnValueOnce(true)
-    writeFileSync.mockReturnValueOnce(true)
-    expect(textWriter("/tmp/new file.txt", "Hello world!")).toBe("/tmp/new-file.txt")
-    
+  parse.mockReturnValueOnce({ name: "new file", ext: ".txt", dir: "/tmp" });
+  slugify.mockReturnValueOnce("new-file");
+  join.mockReturnValueOnce("/tmp/new-file.txt");
+  existsSync.mockReturnValueOnce(true);
+  accessSync.mockReturnValueOnce(true);
+  writeFileSync.mockReturnValueOnce(true);
+  expect(textWriter("/tmp/new file.txt", "Hello world!")).toBe(
+    "/tmp/new-file.txt"
+  );
 
-    expect(parse).toBeCalledWith("/tmp/new file.txt")
-    expect(slugify).toBeCalledWith("new file")
-    expect(join).toBeCalledWith("/tmp","new-file.txt")
-    expect(existsSync).toBeCalledWith("/tmp")
+  expect(parse).toBeCalledWith("/tmp/new file.txt");
+  expect(slugify).toBeCalledWith("new file");
+  expect(join).toBeCalledWith("/tmp", "new-file.txt");
+  expect(existsSync).toBeCalledWith("/tmp");
 
-    expect(accessSync).toBeCalledWith('/tmp', W_OK)
-    expect(writeFileSync).toBeCalledWith("/tmp/new-file.txt", "Hello world!")
-})
+  expect(accessSync).toBeCalledWith("/tmp", W_OK);
+  expect(writeFileSync).toBeCalledWith("/tmp/new-file.txt", "Hello world!");
+});
--- writers/text-writer.js
+++ writers/text-writer.js
@@ -1,49 +1,35 @@
 const { parse, join } = require("path");
-const { existsSync, accessSync, writeFileSync, constants: { W_OK } } = require("fs");
+const { existsSync } = require("fs");
 const { slugify } = require("../helpers");
+const { hasWriteAccess, writeFile } = require("../helpers/file-helper");
+
+//tarea, eliminar funciones (has and writeFile, ya lo hice) importarlas(ok),
+//y luego hacer que corran los test unitarios, de acuerdo al proceso de importar.
+//test deberían simplificarse. Cada una hacer que le corra.
 
 const getParseFile = (value) => {
-    try {
-        const { name, ext, dir } = parse(value);
-        return [[slugify(name), ext].join(''), dir];
-    } catch(err) {
-        throw new TypeError("Invalid filename argument")
-    }
-}
-const hasWriteAccess = (path) => {
-    let has = true;
-    try {
-        accessSync(path, W_OK)
-    } catch(err) {
-        has = false;
-    }
-    return has;
-}
-
-const writeFile = (filename, data) => {
-    let result = true
-    try {
-        writeFileSync(filename, data);
-    } catch {
-        result = false
-    }
-    return result;
-}
+  try {
+    const { name, ext, dir } = parse(value);
+    return [[slugify(name), ext].join(""), dir];
+  } catch (err) {
+    throw new TypeError("Invalid filename argument");
+  }
+};
 
 const textWriter = (filename, data) => {
-    const [file, path] = getParseFile(filename);
-    const filePath = join(path, file);
-    if(!existsSync(path)) {
-        throw new TypeError("Invalid path argument")
-    }
-    if(!hasWriteAccess(path)) {
-        throw Error("It doesn't have write permissions")
-    }
-    if(!writeFile(filePath, data)) {
-        throw Error("Ups! something wrong happened while writing the file")
-    }
+  const [file, path] = getParseFile(filename);
+  const filePath = join(path, file);
+  if (!existsSync(path)) {
+    throw new TypeError("Invalid path argument");
+  }
+  if (!hasWriteAccess(path)) {
+    throw Error("It doesn't have write permissions");
+  }
+  if (!writeFile(filePath, data)) {
+    throw Error("Ups! something wrong happened while writing the file");
+  }
 
-    return filePath;
+  return filePath;
 };
 
 module.exports = { textWriter };
